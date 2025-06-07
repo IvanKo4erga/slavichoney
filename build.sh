16a6067839ce786d60b3a7f4f0703b2d31ee643a
Kargin Daniil 1-МД-19 <daniilus.k@gmail.com>
2025-06-07 17:10:40 +0300

    pip3 python3

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # Install dependencies with --no-cache-dir to reduce size
-pip install --no-cache-dir -r requirements.txt
+pip3 install --no-cache-dir -r requirements.txt
 
 # Collect static files and apply migrations
-python manage.py collectstatic --noinput
-python manage.py migrate
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
