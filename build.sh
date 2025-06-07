dc6e9bc300de477223b69fef4b9744f9360273c1
Kargin Daniil 1-МД-19 <daniilus.k@gmail.com>
2025-06-07 17:19:19 +0300

    edit build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.10 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
 # Install dependencies with --no-cache-dir to reduce size
 pip3 install --no-cache-dir -r requirements.txt
 
